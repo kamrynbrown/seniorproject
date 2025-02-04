6a80f9347c516de059db5338fe13616b4eac35b2
ashley <90946757+ashleyh12@users.noreply.github.com>
2025-02-04 13:13:12 -0500

    Create VISBTests.swift

---
+++ VISB/VISBTests/VISBTests.swift
@@ -0,0 +1,17 @@
+//
+//  VISBTests.swift
+//  VISBTests
+//
+//  Created by violetedwards on 1/30/25.
+//
+
+import Testing
+@testable import VISB
+
+struct VISBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
